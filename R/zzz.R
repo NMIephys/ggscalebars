c235be57722409bdda4a48a1ebb982d8a24565c3
Timm Dankergit config --global user.email timm.danker@nmi.de <you@example.com>
2025-12-21 22:18:02 +0100

    set defaults for get_file

--- R/zzz.R
+++ R/zzz.R
@@ -3,4 +3,5 @@
   options(ephys4.ROBOO_ptrs_as_lists = TRUE)
   options(ephys4.HAMA_ptrs_as_lists  = TRUE)
   options("ephys4.cursor_annots_not_from_attr"=TRUE)
+  options("data_files_path"=c("_Plates", "DATA"))
 }
